790fd43ecf316c7b65abee7366744ca856e20573
Mohamed Mouyahada <mouyahada@gmail.com>
2024-06-28 22:57:16 +0100

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+package {'python3-pip':
+  ensure => installed,
+}
+
+python::pip { 'flask':
+  ensure   => '2.1.0',
+  pip_provider => 'pip3'
+  require => Package['python3-pip'],
 }
