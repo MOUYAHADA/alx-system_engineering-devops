e3aabf8384cf903b7e7b918a97ba81552ab295f8
Mohamed Mouyahada <mouyahada@gmail.com>
2024-08-14 03:04:35 +0100

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0x16. API advanced
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """A function that returns the number of subscribers
+    to an account if exist"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(
+        url,
+        headers={"User-Agent": "custom"},
+        allow_redirects=False,
+    )
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+    else:
+        return 0
