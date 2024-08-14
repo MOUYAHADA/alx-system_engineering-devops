204a5e7535e694563b8ea5fb14cc482d84ee3bd9
Mohamed Mouyahada <mouyahada@gmail.com>
2024-08-14 03:12:31 +0100

    top ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """A function that returns the number of subscribers
-    to an account if exist"""
+    to an account if existed."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(
         url,
